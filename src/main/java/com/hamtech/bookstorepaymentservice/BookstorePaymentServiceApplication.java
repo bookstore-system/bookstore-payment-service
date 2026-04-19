9d9391af0956caa369e6eb56759e8eb85bef4566
TruongIKPK <phitruong20192020@gmail.com>
2026-04-19 23:13:15 +0700

    first commit

---
+++ src/main/java/com/hamtech/bookstorepaymentservice/BookstorePaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hamtech.bookstorepaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstorePaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstorePaymentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hamtech/bookstorepaymentservice/BookstorePaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamtech.bookstorepaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstorePaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
