46f345e01798d0a50a17752dc244b24946e645ef
Ngô Huy <117896799+huydotcode@users.noreply.github.com>
2026-05-03 01:09:00 +0700

    feat(payment): add config and utility classes for vnPay, zaloPay, and MoMo integrations

---
+++ src/main/java/com/hamtech/bookstorepaymentservice/config/MoMoConfig.java
@@ -0,0 +1,19 @@
+package com.hamtech.bookstorepaymentservice.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "payment.momo")
+@Data
+public class MoMoConfig {
+    private String partnerCode;
+    private String accessKey;
+    private String secretKey;
+    private String endpoint;
+    private String returnUrl;
+    private String ipnUrl;
+    private String requestType;
+    private String extraData;
+}
---
+++ src/main/java/com/hamtech/bookstorepaymentservice/config/ZaloPayConfig.java
@@ -0,0 +1,36 @@
+package com.hamtech.bookstorepaymentservice.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ZaloPayConfig {
+
+    private final ObjectMapper objectMapper;
+
+    @Value("${payment.zaloPay.appId}")
+    String appId;
+    @Value("${payment.zaloPay.key1}")
+    String key1;
+    @Value("${payment.zaloPay.key2}")
+    String key2 ;
+    @Value("${payment.zaloPay.zaloPayUrl}")
+    String endpoint;
+    @Value("${payment.zaloPay.returnUrl}")
+    String returnUrl;
+    @Value("${payment.zaloPay.redirectUrl}")
+    String redirectUrl;
+    @Value("${payment.zaloPay.callbackUrl}")
+    String callbackUrl;
+
+
+}
