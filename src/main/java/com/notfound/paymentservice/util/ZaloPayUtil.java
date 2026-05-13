71afbbabf9a26bb86da501b13b0444ee13e88732
Ngô Huy <117896799+huydotcode@users.noreply.github.com>
2026-05-13 17:55:31 +0700

    fix(payment): update ObjectMapper import in ZaloPayConfig and ZaloPayUtil

--- src/main/java/com/notfound/paymentservice/util/ZaloPayUtil.java
+++ src/main/java/com/notfound/paymentservice/util/ZaloPayUtil.java
@@ -1,6 +1,6 @@
 package com.notfound.paymentservice.util;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.ObjectMapper;
 import com.notfound.paymentservice.config.ZaloPayConfig;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
